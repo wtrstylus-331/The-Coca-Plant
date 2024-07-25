a4ef234a684fcb69ba249405c44dbfa0aff972c1
Sri <saisrikara5@gmail.com>
2024-07-24 22:46:12 -0400

    Add CreativeTab.java and icon

---
+++ src/main/java/com/waterstylus331/cocaleafplant/CreativeTab.java
@@ -0,0 +1,23 @@
+package com.waterstylus331.cocaleafplant;
+
+import com.waterstylus331.cocaleafplant.items.ModItems;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class CreativeTab {
+    public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CocaLeafPlant.MODID);
+
+    public static final RegistryObject<CreativeModeTab> MAIN_TAB = TABS.register("creative_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.TAB_ICON.get()))
+                    .title(Component.translatable("creativetab.creative_tab"))
+                    .build());
+
+    public static void register(IEventBus bus) {
+        TABS.register(bus);
+    }
+}

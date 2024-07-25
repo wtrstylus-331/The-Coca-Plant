e0fab7eba26246d22b1c5a6f78c74262d22272d0
Sri <saisrikara5@gmail.com>
2024-07-25 11:57:49 -0400

    Add plant seeds

--- src/main/java/com/waterstylus331/cocaleafplant/item/ModItems.java
+++ src/main/java/com/waterstylus331/cocaleafplant/item/ModItems.java
@@ -1,7 +1,9 @@
 package com.waterstylus331.cocaleafplant.item;
 
 import com.waterstylus331.cocaleafplant.CocaLeafPlant;
+import com.waterstylus331.cocaleafplant.block.ModBlocks;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemNameBlockItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,6 +14,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> TAB_ICON = ITEMS.register("tab_icon",
             () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> COCA_SEEDS = ITEMS.register("coca_seeds",
+            () -> new ItemNameBlockItem(ModBlocks.COCA_PLANT.get(), new Item.Properties()));
 
     public static void register(IEventBus bus) {
         ITEMS.register(bus);

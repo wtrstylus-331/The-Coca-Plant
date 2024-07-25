27336ecd15809134ba35ca3f4ed3f4bfe8653134
Sri <saisrikara5@gmail.com>
2024-07-25 11:57:44 -0400

    Add plant

--- src/main/java/com/waterstylus331/cocaleafplant/block/ModBlocks.java
+++ src/main/java/com/waterstylus331/cocaleafplant/block/ModBlocks.java
@@ -2,13 +2,19 @@ package com.waterstylus331.cocaleafplant.block;
 
 import com.waterstylus331.cocaleafplant.CocaLeafPlant;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, CocaLeafPlant.MODID);
 
+    public static final RegistryObject<Block> COCA_PLANT = BLOCKS.register("coca_plant",
+            () -> new CocaPlantBlock(BlockBehaviour.Properties.copy(Blocks.WHEAT).noOcclusion().noCollission()));
 
     public static void register(IEventBus bus) {
         BLOCKS.register(bus);

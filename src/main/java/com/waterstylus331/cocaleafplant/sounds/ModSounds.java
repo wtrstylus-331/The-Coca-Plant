69a9b4d6f40d0b8956036d4e99e2435a01538521
Sri <saisrikara5@gmail.com>
2024-07-26 12:34:41 -0400

    Added sounds

---
+++ src/main/java/com/waterstylus331/cocaleafplant/sounds/ModSounds.java
@@ -0,0 +1,21 @@
+package com.waterstylus331.cocaleafplant.sounds;
+
+import com.waterstylus331.cocaleafplant.CocaLeafPlant;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModSounds {
+    public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
+            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, CocaLeafPlant.MODID);
+
+    public static final RegistryObject<SoundEvent> MORTAR_PESTLE_USED = SOUND_EVENTS.register("mortar_pestle_used",
+            () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(CocaLeafPlant.MODID, "mortar_pestle_used")));
+
+    public static void register(IEventBus bus) {
+        SOUND_EVENTS.register(bus);
+    }
+}

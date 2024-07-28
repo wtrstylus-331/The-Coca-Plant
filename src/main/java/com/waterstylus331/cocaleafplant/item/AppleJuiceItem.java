7676c1b9cca410903508756e618d791072f321c6
Sri <saisrikara5@gmail.com>
2024-07-28 18:23:29 -0400

    Added apple juice class

---
+++ src/main/java/com/waterstylus331/cocaleafplant/item/AppleJuiceItem.java
@@ -0,0 +1,62 @@
+package com.waterstylus331.cocaleafplant.item;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
+
+public class AppleJuiceItem extends HoneyBottleItem {
+    public AppleJuiceItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        return ItemUtils.startUsingInstantly(level, player, hand);
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack p_41358_) {
+        return UseAnim.DRINK;
+    }
+
+    @Override
+    public int getUseDuration(ItemStack p_41360_) {
+        return 40;
+    }
+
+    @Override
+    public SoundEvent getDrinkingSound() {
+        return SoundEvents.GENERIC_DRINK;
+    }
+
+    @Override
+    public SoundEvent getEatingSound() {
+        return SoundEvents.GENERIC_DRINK;
+    }
+
+    @Override
+    public ItemStack finishUsingItem(ItemStack itemStack, Level level, LivingEntity entity) {
+        if (entity instanceof Player player) {
+            if (!player.getAbilities().instabuild) {
+                itemStack.shrink(1);
+
+                if (itemStack.isEmpty()) {
+                    return new ItemStack(Items.GLASS_BOTTLE);
+                } else {
+                    ItemStack glassBottle = new ItemStack(Items.GLASS_BOTTLE);
+                    if (!player.getInventory().add(glassBottle)) {
+                        player.drop(glassBottle, false);
+                    }
+                }
+            }
+        }
+
+        return itemStack.isEmpty() ? new ItemStack(Items.GLASS_BOTTLE) : itemStack;
+    }
+}

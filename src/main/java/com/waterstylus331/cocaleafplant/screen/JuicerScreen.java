31da04fc9d94df3b76d3d5fab786c2c277019fcd
Sri <saisrikara5@gmail.com>
2024-07-28 15:11:25 -0400

    Added juicer screen class

---
+++ src/main/java/com/waterstylus331/cocaleafplant/screen/JuicerScreen.java
@@ -0,0 +1,51 @@
+package com.waterstylus331.cocaleafplant.screen;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.waterstylus331.cocaleafplant.CocaLeafPlant;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+
+public class JuicerScreen extends AbstractContainerScreen<JuicerMenu> {
+    private static final ResourceLocation TEXTURE =
+            new ResourceLocation(CocaLeafPlant.MODID, "textures/gui/juicer_gui.png");
+
+    public JuicerScreen(JuicerMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
+        super(pMenu, pPlayerInventory, pTitle);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        this.inventoryLabelY = 10000;
+    }
+
+    @Override
+    protected void renderBg(GuiGraphics guiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) / 2;
+
+        guiGraphics.blit(TEXTURE, x, y, 0, 0, imageWidth, imageHeight);
+
+        renderProgressArrow(guiGraphics, x, y);
+    }
+
+    private void renderProgressArrow(GuiGraphics guiGraphics, int x, int y) {
+        if(menu.isCrafting()) {
+            guiGraphics.blit(TEXTURE, x + 80, y + 31, 176, 0, menu.getScaledProgress(), 8);
+        }
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
+        renderBackground(guiGraphics);
+        super.render(guiGraphics, mouseX, mouseY, delta);
+        renderTooltip(guiGraphics, mouseX, mouseY);
+    }
+}

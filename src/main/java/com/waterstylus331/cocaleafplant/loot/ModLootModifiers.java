d3e8f473aea22ee9dfa818e98ac9f62153df9941
Sri <saisrikara5@gmail.com>
2024-08-01 13:23:22 -0400

    Add loot modifier for coca seeds

---
+++ src/main/java/com/waterstylus331/cocaleafplant/loot/ModLootModifiers.java
@@ -0,0 +1,22 @@
+package com.waterstylus331.cocaleafplant.loot;
+
+import com.mojang.serialization.Codec;
+import com.waterstylus331.cocaleafplant.CocaLeafPlant;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModLootModifiers {
+    public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIER_SERIALIZERS =
+            DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, CocaLeafPlant.MODID);
+
+    public static final RegistryObject<Codec<? extends IGlobalLootModifier>> ADD_ITEM =
+            LOOT_MODIFIER_SERIALIZERS.register("add_item", AddModifier.CODEC);
+
+
+    public static void register(IEventBus bus) {
+        LOOT_MODIFIER_SERIALIZERS.register(bus);
+    }
+}
